34c991504d3b1cef97df1ffb8bb83ee90d116299
郭建林 <1050070285@qq.com>
2020-10-21 11:25:04 +0800

    完成tabs栏目和子组件的迁移

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,127 +1,52 @@
 <template>
   <div class="home">
 
-
-      <div class="carousel">
-        <el-carousel :interval="4000" type="card" height="300px">
-          <el-carousel-item v-for="(item,index) in bannersList" :key="index">
-            <img :src="item.imageUrl" alt="">
-          </el-carousel-item>
-        </el-carousel>
-      </div>
-      <div class="getRecommendedSongList">
-        <div class="content">
-          <h5 style="text-align: left;">推荐歌单</h5>
-          <el-divider />
-          <div v-for="(item,index) in songList" :key="index" class="songList">
-            <img :src="item.picUrl" alt="">
-            <p class="name">{{ item.name }}</p>
-          </div>
-        </div>
-      </div>
-      <div class="djprogramList">
-        <h5 style="text-align: left;">推荐电台</h5>
-        <el-divider />
-        <div v-for="(item,index) in djprogramList" :key="index" class="songList">
-          <img :src="item.program.blurCoverUrl" alt="">
-          <p class="name">{{ item.name }}</p>
-        </div>
-      </div>
-
-
+    <el-tabs v-model="activeName" stretch @tab-click="handleClick">
+      <el-tab-pane label="个性推荐" name="first">
+        <personalized-recommendation />
+      </el-tab-pane>
+      <el-tab-pane label="歌单" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="主播电台" name="third">配置管理</el-tab-pane>
+      <el-tab-pane label="歌手" name="fourth">角色管理</el-tab-pane>
+      <el-tab-pane label="排行榜" name="five">定时任务补偿</el-tab-pane>
+      <el-tab-pane label="最新音乐" name="six">定时任务补偿</el-tab-pane>
+    </el-tabs>
 
   </div>
 </template>
 
 <script>
-import { Home } from '../api/home'
+import PersonalizedRecommendation from '@/views/Home/PersonalizedRecommendation'
 
 export default {
   name: 'Home',
   components: {
-
+    PersonalizedRecommendation
   },
   data() {
     return {
-      bannersList: [],
-      songList: [],
-      djprogramList: []
+      activeName: 'first'
     }
   },
   created() {
-    this.feach()
+
   },
   methods: {
-    async feach() {
-      const { banners } = await Home.getHomeBanner()
-      this.bannersList = banners
-
-      // 推荐歌单
-      const { result } = await Home.getRecommendedSongList()
-      this.songList = result
-
-      // 推荐电台
-      const data = await Home.getRecommendedDjprogram()
-      this.djprogramList = data.result
-    }
+    handleClick() {}
   }
 }
 </script>
 <style scoped lang="scss">
   .home{
-    .carousel{
-      width: 95%;
+    width: 100%;
+    .el-tabs{
       margin: 0 auto;
     }
-    .getRecommendedSongList{
-      width: 100%;
-      margin: 0 auto;
-      .content{
-        width: 95%;
-        margin: 0 auto;
-        .songList{
-          width: 12%;
-          height: 20%;
-          position: relative;
-          display: inline-block;
-          margin-left: 2%;
-          margin-top: 2%;
-          img{
-            width: 100%;
-            height: 80%;
-            border-radius: 2%;
-            margin: 0 auto;
-            display: inline-block;
-          }
-          .name{
-            display: inline-block;
-          }
-        }
-
-      }
-
+    .el-tabs__item.is-active {
+      color: red !important;
     }
-    .djprogramList{
-      margin-top: 2%;
-      width: 100%;
-      margin-left: auto;
-      margin-right: auto;
-      .songList{
-        width: 12%;
-        height: 20%;
-        display: inline-block;
-        margin-left: 4%;
-        img{
-          width: 100%;
-          height: 80%;
-          border-radius: 2%;
-          margin: 0 auto;
-          display: inline-block;
-        }
-        .name{
-          display: inline-block;
-        }
-      }
+    .el-tabs__nav-scroll{
+      width: 50%;
     }
   }
 
---
+++ src/views/Home/PersonalizedRecommendation.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="home">
+    <div class="carousel">
+      <el-carousel :interval="4000" type="card" height="300px">
+        <el-carousel-item v-for="(item,index) in bannersList" :key="index">
+          <img :src="item.imageUrl" alt="">
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="getRecommendedSongList">
+      <div class="content">
+        <h5 style="text-align: left;">推荐歌单</h5>
+        <el-divider />
+        <div v-for="(item,index) in songList" :key="index" class="songList">
+          <img :src="item.picUrl" alt="">
+          <p class="name">{{ item.name }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="djprogramList">
+      <h5 style="text-align: left;">推荐电台</h5>
+      <el-divider />
+      <div v-for="(item,index) in djprogramList" :key="index" class="songList">
+        <img :src="item.program.blurCoverUrl" alt="">
+        <p class="name">{{ item.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Home } from '../../api/home'
+
+export default {
+  name: 'PersonalizedRecommendation',
+  components: {
+
+  },
+  data() {
+    return {
+      bannersList: [],
+      songList: [],
+      djprogramList: []
+    }
+  },
+  created() {
+    this.feach()
+  },
+  methods: {
+    async feach() {
+      const { banners } = await Home.getHomeBanner()
+      this.bannersList = banners
+
+      // 推荐歌单
+      const { result } = await Home.getRecommendedSongList()
+      this.songList = result
+
+      // 推荐电台
+      const data = await Home.getRecommendedDjprogram()
+      this.djprogramList = data.result
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .home{
+    .carousel{
+      width: 95%;
+      margin: 0 auto;
+    }
+    .getRecommendedSongList{
+      width: 100%;
+      margin: 0 auto;
+      .content{
+        width: 95%;
+        margin: 0 auto;
+        .songList{
+          width: 12%;
+          height: 20%;
+          position: relative;
+          display: inline-block;
+          margin-left: 2%;
+          margin-top: 2%;
+          img{
+            width: 100%;
+            height: 80%;
+            border-radius: 2%;
+            margin: 0 auto;
+            display: inline-block;
+          }
+          .name{
+            display: inline-block;
+          }
+        }
+
+      }
+
+    }
+    .djprogramList{
+      margin-top: 2%;
+      width: 100%;
+      margin-left: auto;
+      margin-right: auto;
+      .songList{
+        width: 12%;
+        height: 20%;
+        display: inline-block;
+        margin-left: 4%;
+        img{
+          width: 100%;
+          height: 80%;
+          border-radius: 2%;
+          margin: 0 auto;
+          display: inline-block;
+        }
+        .name{
+          display: inline-block;
+        }
+      }
+    }
+  }
+
+</style>
+
